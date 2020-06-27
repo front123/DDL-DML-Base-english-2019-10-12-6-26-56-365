5b4e7e0a26299b2c7507ee5c185527902a607560
front <34743160+front123@users.noreply.github.com>
2020-06-27 11:17:44 +0800

    Update dml.sql

--- dml.sql
+++ dml.sql
@@ -1,7 +1,11 @@
 -- Insert record
+insert into teacher(id, name) values(101, 'Mike');
 
 -- Revise record
+update teacher set name='John' where id=101;
 
 --  Delete record
+delete from teacher where id=101;
 
 -- Search record
+select * from teacher where id=101;
